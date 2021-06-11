6930e3b895e57c291a8f06fcf88d795c902294b6
Joshua Raphael Fuentes <joshuarpf@gmail.com>
2021-06-11 17:37:39 +0800

    added events to buttons

--- webtoe/src/views/Cell/index.tsx
+++ webtoe/src/views/Cell/index.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const style = {
+	width: '50px',
+	height: '50px',
+};
 
 const Cell = () => {
+	const [cellValue, setCellValue] = useState('')
+
+	function handleClick(value: string) {
+		setCellValue(value)
+	}
+
 	return (
-		<div>
-			X/0
+		<div style={style}>
+			<h1>{cellValue}</h1>
+			<button onClick={handleClick.bind(this, 'x')}>X</button>
+			<button onClick={handleClick.bind(this, 'o')}>0</button>
 		</div>
 	)
 }
